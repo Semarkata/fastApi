2974fbb230a385631cc8a9da892f7b70c67585d6
Semarkata <semarkata@gmail.com>
2024-01-05 21:14:39 +0100

    Add doc

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title='Fast Api',
+    description='My Fast Api project',
+    version='0.0.1',
+)
 
-@app.get('/')
+@app.get('/', tags=['Root'])
 
 def read_root():
     return {"Hello": "World"}
