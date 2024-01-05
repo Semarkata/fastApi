0c5063215baa012ad3fea2bf00f5ab216ecc2eed
Semarkata <semarkata@gmail.com>
2024-01-05 23:38:25 +0100

    Create CRUD

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from http import HTTPStatus as http_response
+import mockdata
 
 app = FastAPI(
     title='Fast Api',
@@ -7,6 +9,71 @@ app = FastAPI(
 )
 
 @app.get('/', tags=['Root'])
-
 def read_root():
     return {"Hello": "World"}
+
+@app.get('/movies', tags=['Movies'])
+def get_movies():
+    return mockdata.movies
+
+@app.get('/movies/{id}', tags=['Movies'])
+def get_movie(id: int):
+    for movie in mockdata.movies:
+        if movie['id'] == id:
+            return movie
+    return 'No Movie Found'
+
+@app.get('/movies/', tags=['Movies'])
+def get_movies_by_category(category: str):
+    for movie in mockdata.movies:
+        if movie['category'] == category:
+            return movie
+    return 'No Movie Found'
+
+@app.post('/movies', tags=['Movies'])
+def create_movie(
+        id: int = Body(),
+        title: str = Body(),
+        overview: str = Body(),
+        release_date: str = Body(),
+        rating: float = Body(),
+        category: str = Body(),
+):
+    mockdata.movies.append({
+        'id': id,
+        'title': title,
+        'overview': overview,
+        'release_date': release_date,
+        'rating': rating,
+        'category': category
+    })
+    return http_response.CREATED
+
+@app.put('/movies/{id}', tags=[''])
+def update_movie(
+        id: int,
+        title: str = Body(),
+        overview: str = Body(),
+        release_date: str = Body(),
+        rating: float = Body(),
+        category: str = Body(),
+):
+    for movie in mockdata.movies:
+        if movie['id'] == id:
+            movie['title'] = title
+            movie['overview'] = overview
+            movie['release_date'] = release_date
+            movie['rating'] = rating
+            movie['category'] = category
+            return http_response.OK
+        else:
+            return http_response.NOT_FOUND
+
+@app.delete('/movies/{id}', tags=[''])
+def delete_movie(id:int):
+    for movie in mockdata.movies:
+        if movie['id'] == id:
+            mockdata.movies.remove(movie)
+            return http_response.OK
+        else:
+            return http_response.NOT_FOUND
---
+++ mockdata.py
@@ -0,0 +1,10 @@
+movies = [
+    {
+        'id': 1,
+        'title': 'Movie 1',
+        'overview': 'Movie',
+        'release_date': '1970-01-01',
+        'rating': '9.2',
+        'category': "Movie category"
+    }
+]
